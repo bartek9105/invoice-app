8a20c8489b8e56a0958ae25e35cd9dbd20acde2e
bartek9105 <bartek9105@gmail.com>
2021-03-01 21:55:06 +0100

    Create barebones invoices view structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="container">
+    <header class="flex-sb">
+      <div class="flex-col">
+        <h1>Invoices</h1>
+        <span>There are 7 total invoices</span>
+      </div>
+      <div>
+        Dropdown Button
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
