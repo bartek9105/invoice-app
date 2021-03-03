a501aede9e2dd83410f3a9e38fe993613a826f94
bartek9105 <bartek9105@gmail.com>
2021-03-03 21:42:12 +0100

    Create Add Button component, add scss utility classes

---
+++ src/components/AddButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="btn btn-purple btn-add flex-sb">
+    <img src="@/assets/icons/plus.svg" alt="Add" class="mr-16" />
+    New invoice
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AddButton'
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,15 @@
         <h1 class="mb-16">Invoices</h1>
         <span class="font-12 gray">There are 7 total invoices</span>
       </div>
-      <div class="status-filter">
-        <span class="bold font-12 mr-16" @click="displayDropdown = !displayDropdown">
-          Filter by status
-        </span>
-        <img src="@/assets/icons/arrow.svg" />
-        <Dropdown v-if="displayDropdown" class="dropdown" />
+      <div class="flex-sb">
+        <div class="status-filter mr-40">
+          <span class="bold font-12 mr-16" @click="displayDropdown = !displayDropdown">
+            Filter by status
+          </span>
+          <img src="@/assets/icons/arrow.svg" />
+          <Dropdown v-if="displayDropdown" class="dropdown" />
+        </div>
+        <AddButton />
       </div>
     </header>
   </div>
@@ -18,11 +21,13 @@
 
 <script>
 import Dropdown from '@/components/Dropdown.vue'
+import AddButton from '@/components/AddButton.vue'
 
 export default {
   name: 'Home',
   components: {
-    Dropdown
+    Dropdown,
+    AddButton
   },
   data() {
     return {
