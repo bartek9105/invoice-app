b9f1c29f4f3d9e84006e6b682b55419ad8d54afc
bartek9105 <bartek9105@gmail.com>
2021-03-03 21:28:44 +0100

    Create dropdown component, create necessary scss utility classes

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dropdown flex-col">
+    <div class="mb-16">
+      <input id="draft" type="checkbox" name="draft" class="mr-12" />
+      <label for="draft">Draft</label>
+    </div>
+    <div class="mb-16">
+      <input id="pending" type="checkbox" name="pending" class="mr-12" />
+      <label for="pending">Pending</label>
+    </div>
+    <div>
+      <input id="Paid" type="checkbox" name="Paid" class="mr-12" />
+      <label for="Paid">Paid</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown'
+}
+</script>
+
+<style lang="scss" scoped>
+.dropdown {
+  background-color: #fff;
+  box-shadow: $shadow;
+  border-radius: 8px;
+  font-size: $font-sm;
+  font-weight: $font-bold;
+  padding: 24px;
+  width: 192px;
+  z-index: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,44 @@
   <div class="container">
     <header class="flex-sb">
       <div class="flex-col">
-        <h1>Invoices</h1>
-        <span>There are 7 total invoices</span>
+        <h1 class="mb-16">Invoices</h1>
+        <span class="font-12 gray">There are 7 total invoices</span>
       </div>
-      <div>
-        Dropdown Button
+      <div class="status-filter">
+        <span class="bold font-12 mr-16" @click="displayDropdown = !displayDropdown">
+          Filter by status
+        </span>
+        <img src="@/assets/icons/arrow.svg" />
+        <Dropdown v-if="displayDropdown" class="dropdown" />
       </div>
     </header>
   </div>
 </template>
 
 <script>
+import Dropdown from '@/components/Dropdown.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Dropdown
+  },
+  data() {
+    return {
+      displayDropdown: false
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.status-filter {
+  position: relative;
+  cursor: pointer;
+}
+.dropdown {
+  position: absolute;
+  top: 48px;
+  left: -24px;
+}
+</style>
