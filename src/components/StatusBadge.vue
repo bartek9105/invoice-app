2055cbf1d0ddc22814f35703b41b2f7eaf892ef8
bartek9105 <bartek9105@gmail.com>
2021-03-07 20:21:31 +0100

    Create status badge component and use it in invoice list item and invoice details view

---
+++ src/components/StatusBadge.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="status-badge" :class="statusColor">
+    <svg
+      class="status-badge__icon"
+      width="8"
+      height="8"
+      viewBox="0 0 8 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="4" cy="4" r="4" :fill="iconColor" />
+    </svg>
+    {{ status }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatusBadge',
+  props: {
+    status: {
+      type: String,
+      default: 'Not specified'
+    }
+  },
+  computed: {
+    statusColor() {
+      if (this.status === 'paid') {
+        return 'green bg-green'
+      }
+      if (this.status === 'pending') {
+        return 'orange bg-orange'
+      }
+      if (this.status === 'draft') {
+        return 'gray bg-gray'
+      }
+      return ''
+    },
+    iconColor() {
+      if (this.status === 'paid') {
+        return '#33D69F'
+      }
+      if (this.status === 'pending') {
+        return 'orange'
+      }
+      if (this.status === 'draft') {
+        return 'black'
+      }
+      return ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.status-badge {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: $font-bold;
+  width: 104px;
+  height: 40px;
+  border-radius: 0.375rem;
+  &__icon {
+    margin-right: 0.5rem;
+  }
+}
+</style>
--- src/views/InvoiceDetails.vue
+++ src/views/InvoiceDetails.vue
@@ -3,16 +3,25 @@
     <router-link to="/">
       <BackButton />
     </router-link>
+    <header class="header flex-sb-c">
+      <div class="header__status">
+        Status
+        <StatusBadge />
+      </div>
+      <div>Buttons</div>
+    </header>
   </div>
 </template>
 
 <script>
 import BackButton from '@/components/BackButton.vue'
+import StatusBadge from '@/components/StatusBadge.vue'
 
 export default {
   name: 'InvoiceDetails',
   components: {
-    BackButton
+    BackButton,
+    StatusBadge
   },
   props: {
     id: String
@@ -20,4 +29,18 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.header {
+  background-color: $white;
+  box-shadow: $shadow;
+  padding: 1.25rem 2rem;
+  border-radius: 0.5rem;
+  margin-top: 2rem;
+  &__status {
+    display: flex;
+    align-items: center;
+    font-size: $font-sm;
+    color: $violet-gray;
+  }
+}
+</style>
