309eb7a3c82ba996f830f92de445f15f44db3ecb
bartek9105 <bartek9105@gmail.com>
2021-03-07 20:04:01 +0100

    Create go back button component and link it to home view

---
+++ src/components/BackButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="back-btn">
+    <img src="@/assets/icons/arrow.svg" alt="back" class="back-btn__icon" />
+    <span>Go back</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackButton'
+}
+</script>
+
+<style lang="scss">
+.back-btn {
+  font-size: $font-sm;
+  font-weight: $font-bold;
+  color: $black;
+  cursor: pointer;
+  &__icon {
+    transform: rotate(90deg);
+    margin-right: 1.5rem;
+  }
+}
+</style>
--- src/views/InvoiceDetails.vue
+++ src/views/InvoiceDetails.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="container">{{ id }}</div>
+  <div class="container">
+    <router-link to="/">
+      <BackButton />
+    </router-link>
+  </div>
 </template>
 
 <script>
+import BackButton from '@/components/BackButton.vue'
+
 export default {
   name: 'InvoiceDetails',
+  components: {
+    BackButton
+  },
   props: {
     id: String
   }
